46ddb520451c4f4f48b84be2c0873249d260cd3f
hoang man <hoangman772@gmail.com>
2021-05-21 16:54:08 +0700

    fix bug mongo success

--- src/db/mongo.go
+++ src/db/mongo.go
@@ -2,25 +2,22 @@ package db
 
 import (
 	"go-mongo-template/src/config"
-	"gopkg.in/mgo.v2"
 	"log"
+
+	"gopkg.in/mgo.v2"
 )
 
 var instance *mgo.Session
 
 var err error
 
-func GetInstance(c *config.Configuration) *mgo.Session  {
+func GetInstance(c *config.Configuration) *mgo.Session {
 	if instance == nil {
 		instance, err = mgo.Dial(c.MongoUrl)
-
 		if err != nil {
 			panic(err)
 		}
-
-		log.Fatalln("😺 mongo connect success 😋!")
+		log.Println("😺 mongo connect success 😋!")
 	}
-
 	return instance.Copy()
 }
-
